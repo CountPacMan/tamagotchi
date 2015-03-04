a1e92c17d5d2626443f08b4199d6b0ae8bb54bfe
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-03-04 15:43:09 -0800

    Fix twig bug

--- views/creature.php
+++ views/creature.php
@@ -4,9 +4,19 @@
     </head>
     <body>
         <h1>^_^</h1>
-
         <form action="/creature" method="post">
-            
+            <button name="food" type="submit">Feed Me!</button>
+            <p>
+                Hunger: {{ creature[0].getFood }}
+            </p>
+            <button name="rest" type="submit">I'm Tired!</button>
+            <p>
+                Sleep: {{ creature[0].getRest }}
+            </p>
+            <button name="play" type="submit">Play with me!</button>
+            <p>
+                Happiness: {{ creature[0].getMood }}
+            </p>
         </form>
     </body>
 </html>
