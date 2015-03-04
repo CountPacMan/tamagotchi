bdf60cea1f696c1596e8eaf00df3c0b249050879
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-03-04 15:20:42 -0800

    Add class source files

---
+++ src/Tamagotchi.php
@@ -0,0 +1,94 @@
+<?php
+
+class Tamagotchi
+{
+    //a value of 0 or 10 would kill the tamagotchi
+    private $rest;
+    private $food;
+    private $mood;
+
+    //setting food is required, rest and mood default to 5
+    function __construct($food, $rest = 5, $mood = 5)
+    {
+        $this->food = $food;
+        $this->rest = $rest;
+        $this->mood = $mood;
+    }
+
+    function getRest()
+    {
+        return $this->rest;
+    }
+
+    function getFood()
+    {
+        return $this->food;
+    }
+
+    function getMood()
+    {
+        return $this->mood;
+    }
+
+    function rest()
+    {
+        $this->rest++;
+        $this->food--;
+    }
+
+    function feed()
+    {
+        $this->food++;
+        $this->rest--;
+    }
+
+    function play()
+    {
+        $this->mood++;
+        $this->food--;
+        $this->rest--;
+    }
+
+    function kick()
+    {
+        $this->mood -= 3;
+    }
+
+    function passTime()
+    {
+        $this->food--;
+        $this->mood--;
+        $this->rest--;
+    }
+
+    function isAlive()
+    {
+        if ($this->food == 0) {
+            return "hunger";
+        } else if ($this->mood == 0) {
+            return "suicide";
+        } else if ($this->rest == 0) {
+            return "sleep deprivation";
+        } else if ($this->food == 10) {
+            return "exploded";
+        } else if ($this->rest == 10) {
+            return "going into permanent coma";
+        } else if ($this->mood == 10) {
+            return "exploded from happiness";
+        } else {
+            return "alive";
+        }
+    }
+
+    function save()
+    {
+        array_push($_SESSION['tamagotchi'], $this);
+    }
+
+    static function getCreature()
+    {
+        return $_SESSION['tamagotchi'];
+    }
+}
+
+ ?>
