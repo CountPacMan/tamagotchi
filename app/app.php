d36906a6cd5e9ff4e8f0eee178eab32dcdf190e0
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-03-04 15:20:29 -0800

    Add session vars and routes

--- app/app.php
+++ app/app.php
@@ -1,11 +1,31 @@
 <?php
 
     require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Tamagotchi.php';
+    session_start();
+    if (empty($_SESSION['tamagotchi'])) {
+        $_SESSION['tamagotchi'] = [];
+    }
 
     $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-    $app->get("/", function() {
-        return "HOME";
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.php');
+    });
+
+    $app->post("/creature", function() use ($app) {
+
+        if(empty($_SESSION['tamagotchi']))
+        {
+            $creature = new Tamagotchi($_POST['weight']);
+            $creature->save();
+        }
+
+        
+
+
+        $app['twig']->render('creature.php', array('creature' => Tamagotchi::getCreature()));
     });
 
 
