98807342e5e25c1157721cb70bc14e48de10c26c
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-03-04 16:18:47 -0800

    Add death page

---
+++ views/died.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Death!</title>
+    </head>
+    <body>
+        <h1>X_X</h1>
+        <p>Your tamagochi died of {{ creature[0].isAlive }}</p>
+        <a href="/">Creature a new Pet!</a>
+    </body>
+</html>
