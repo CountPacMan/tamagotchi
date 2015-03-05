2a889fe861bb83bdf1544ccb2e7cb0f020ad0a65
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-03-04 16:19:17 -0800

    create button actions

--- app/app.php
+++ app/app.php
@@ -18,9 +18,26 @@
     $app->post("/creature", function() use ($app) {
         if(empty($_SESSION['tamagotchi']))
         {
-            echo "Is Empty";
             $creature = new Tamagotchi($_POST['weight']);
             $creature->save();
+        }
+        $creature = Tamagotchi::getCreature();
+        // test to see what button was pressed
+        if (key($_POST) == "food") {
+            $creature[0]->feed();
+        } elseif (key($_POST) == "rest") {
+            $creature[0]->rest();
+        } elseif (key($_POST) == "play") {
+            $creature[0]->play();
+        } elseif (key($_POST) == "time") {
+            $creature[0]->passTime();
+        } elseif (key($_POST) == "kick") {
+            $creature[0]->kick();
+        }
+
+        // test to see if creature died
+        if ($creature[0]->isAlive() != "alive") {
+            return $app['twig']->render('died.php', array('creature' => Tamagotchi::getCreature()));
         }
 
         return $app['twig']->render('creature.php', array('creature' => Tamagotchi::getCreature()));
